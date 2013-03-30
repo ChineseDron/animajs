432d2a5f7c4c9d6390e3d35a3b1d980647b9eab8
Joel <joecantillo@gmail.com>
2013-03-30 02:09:20 +0100

    Add browserPrefix and cssToStr method

--- js/animajs.js
+++ js/animajs.js
@@ -1,33 +1,70 @@
 //(function() {
 function _(el) {
 
+	this.self = this;
+
 	if (window === this) {
     	return new _(el);
 	}
+	var prfix = '' , 
+		prfixKey = '';
 
 	this.selector = el;
 	this.el = document.querySelectorAll(el);
+	
+
+	this.browserPrefix();
+
+	//Private Methods
+	
 	return this;
 }
 
 
 _.prototype = {
 	
+	//@return Browser Prefix
 	browserPrefix : function () {
+	
+		var prefixList = ["Moz" , "Webkit", "O"];
+
+		for( var n = 0 ;n < prefixList.length; n ++ ) {
+			var prefixTemp = prefixList[n] + "AnimationName";
 		
-		var prefixList = ["" , "moz" , "webkit", "o"];
+			if( this.el[0].style[prefixTemp] !== undefined ) {
+				this.prfix = prefixList[n];
+				this.prfixKey = '@-' + this.prfix.toLowerCase() + '-keyframes ';
+			}
+		}
 
 	},
 
+	//@ return just the element selector
 	sel : function () { return this.el },
 
-	animate : function (obj,speed) {
-		for( var n = 0 ;n < this.el.length ; n ++ ) {
+	cssToStr : function (obj) {
+		var cssTemp = "";
+		for (var key in obj) {
+			cssTemp += key + ':' + obj[key] + ';'
+		}
+		
+		return cssTemp;
 
+	},
+
+	//@void - Prepare everything to do the animation part
+	animate : function (obj,speed) {
+		//this.self.cssToStr();
+		for( var n = 0 ;n < this.el.length ; n ++ ) {
+			//console.log( this.self.cssToStr(obj) );
 			//console.log(this.el[n].getAttribute("id"));
-			//this.el[n].style.WebkitAnimation="nextMove 2s ease-in-out 0s 1 alternate"
-			var keyframes  = '@-webkit-keyframes nextMove {' +
-							 'to { left : 300px; } }' ;
+
+			this.el[n].style[this.prfix + 'Animation'] = 
+				"nextMove " + speed + "ms" +
+				" ease-in-out 0s 1 normal forwards";
+			
+			var keyframes  = this.prfixKey + 'nextMove {' +
+							 'to {' + this.cssToStr(obj) + '} }' ;
 			
 			var addStyle = document.createElement("style");
 			addStyle.innerHTML = keyframes;
@@ -35,7 +72,6 @@ _.prototype = {
 			
 		}
 
-		return this;
 	},
 
 
