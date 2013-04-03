b59dba08f0258a57b31b64deb28c4171be5b7b85
Joel <joecantillo@gmail.com>
2013-04-04 01:23:48 +0200

    Add method updateCss, DOM8 bug still

--- js/animajs.js
+++ js/animajs.js
@@ -15,11 +15,9 @@ function _(el) {
 
 	this.browserPrefix();
 
-	//Private Methods
 	
 	return this;
 }
-
 
 _.prototype = {
 	
@@ -40,53 +38,108 @@ _.prototype = {
 
 	},
 
+	//@void Update the css after the animation
+	updateCss : function (el,obj) {
+		el.style.cssText = this.cssToStr(obj,true);
+	},
+
 	//@ return just the element selector
 	sel : function () { return this.el },
 
-	cssToStr : function (obj) {
+	//@ Transform an object with css data into a string
+	cssToStr : function (obj,camel) {
+		
+		camel = (typeof camel !== 'undefined') ? camel : false;
 		var cssTemp = "";
+		
 		for (var key in obj) {
-			cssTemp += key + ':' + obj[key] + ';'
+			
+			if (camel === false) cssTemp += key + ':' + obj[key] + ';' ;
+				else cssTemp += cssCamel(key) + ':' + obj[key] + ';' ;
+		
 		}
 		
 		return cssTemp;
 
 	},
 
-	//@void - Prepare everything to do the animation part
-	animate : function (obj,speed) {
+	//This function decided how we should end the trans/anim, deleting his keyframes or styles
+	transitionEnd : function (el, addStyle, style) {
+		var self = this.self;
+		var animationEnd = '';
+
+		switch (this.prfix) {
+			case "Moz" :
+				animationEnd = "transitionend";
+			break;
+
+			case "Webkit" :
+				animationEnd = "webkitAnimationEnd";
+			break;
+
+			case "O" :
+				animationEnd = "transitionend";
+			break;
+		}
+
+		el.addEventListener( 
+     			
+ 			animationEnd, 
+ 			
+ 			function( event ) { 
+ 				this.style[self.prfix + 'Animation'] = '';
+     			document.getElementsByTagName( 'head' )[ 0 ].removeChild( addStyle );
+     			//alert( "Finished transition!" ); 
+ 				self.updateCss(this,style);
+ 			}, false 
+
+     	);
+	},
+
+	//@void - Prepare everything to do the animation part, This should work like jQuery Animations
+	animate : function (style,speed) {
 		//this.self.cssToStr();
 		for( var n = 0 ;n < this.el.length ; n ++ ) {
-			//console.log( this.self.cssToStr(obj) );
-			//console.log(this.el[n].getAttribute("id"));
-			self = this.el[n];
+		
+			self = this.self;
 
 			this.el[n].style[this.prfix + 'Animation'] = 
 				"nextMove " + speed + "ms" +
 				" ease-in-out 0s 1 normal forwards";
 			
 			var keyframes  = this.prfixKey + 'nextMove {' +
-							 'to {' + this.cssToStr(obj) + '} }' ;
+							 'to {' + this.cssToStr(style) + '} }' ;
 			
 			var addStyle = document.createElement("style");
 			addStyle.innerHTML = keyframes;
 			document.getElementsByTagName( 'head' )[ 0 ].appendChild( addStyle );
 			
-			this.el[n].addEventListener( 
-     			'webkitAnimationEnd', 
-     			
-     			function( event ) { 
-     				self.style.WebkitAnimation = '';
-         			document.getElementsByTagName( 'head' )[ 0 ].removeChild( addStyle );
-         			//alert( "Finished transition!" ); 
-     			}, false 
-     		);
-					
+			this.transitionEnd( this.el[n],addStyle,style );
+							
 		}
 
 	},
 
 
+}
+
+//Private Methods	
+
+function cssCamel(obj) {
+	
+	var index = obj.split('-');
+
+	if (index.length > 1) {
+		
+		var temp = index[0];
+	
+		for( var n = 1; n < index.length ; n ++ ) {
+			temp +=  index[n][0].toUpperCase() + index[n].slice(1);
+		}
+
+		return temp;
+	}
+	return obj;
 }
 
 //})(window);
